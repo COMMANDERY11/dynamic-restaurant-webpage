c339dfc06ad07e0f23ae726eeb7d9443ec0b2130
Izayah <izayah5002@gmail.com>
2024-02-06 15:24:17 -0500

    Add src/

---
+++ src/home.js
@@ -0,0 +1,15 @@
+function homeStyle() {
+   const heroContainer = createElement("div");
+   heroContainer.id = "heroContainer";
+   content.appendChild(heroContainer);
+
+   const heroHeader = createElement("h1");
+   heroHeader.id = "heroHeader";
+   heroContainer.appendChild(heroHeader);
+
+   const heroSlogan = createElement("h2");
+   heroSlogan.id = "heroSlogan";
+   heroHeader.appendChild(heroSlogan);
+}
+
+export { homeStyle };
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './style.scss';
+
+const content = querySelector("#content");
+let currentTab = 0;
+
+switch (currentTab) {
+   case 0:
+      import { homeStyle } from "./home.js";
+      break;
+
+   case 1:
+      break;
+
+   case 2:
+      break;
+}
